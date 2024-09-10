ab4cf6b91466d83053358cf9a726e364d9b37556
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-10 18:31:17 +0000

    login como aluno

--- scenario1/background.sh
+++ scenario1/background.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "teste 0"
 
 # cria o usuário aluno com a pasta home
 sudo useradd -m -s /bin/bash aluno
@@ -13,16 +12,10 @@ echo "aluno ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/aluno
 # adicionar o usuário ao grupo de 'sudo'
 sudo usermod -aG sudo aluno
 
-echo "teste 1"
-
 # ajustar as permissões
 sudo chmod 0440 /etc/sudoers.d/aluno
 
-echo "teste 2"
-
 # faz login como 'aluno'
-sudo su - aluno
-
-echo "teste 3"
+echo "sudo su - aluno" >> /etc/profile
 
 exit 0
