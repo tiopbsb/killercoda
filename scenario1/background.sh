2b98bd66c40a3faeeec829b41eeef13554476705
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-10 17:37:48 +0000

    login do usuário

--- scenario1/background.sh
+++ scenario1/background.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
+# cria o usuário aluno com a pasta home
 sudo useradd -m -s /bin/bash aluno
 
-echo "aluno:aluno" | sudo chpasswd
+# definir a senha para o usuário aluno
+echo "aluno:senha" | sudo chpasswd
 
+# desabilitar pedido de senha para comando sudo
+echo "aluno ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/aluno
+
+# adicionar o usuário ao grupo de 'sudo'
 sudo usermod -aG sudo aluno
 
+# ajustar as permissões
 sudo chmod 0440 /etc/sudoers.d/aluno
+
+# faz login como 'aluno'
+su - aluno
 
 exit 0
