70c8e51e812ebd0cc6ab1c81b196b7e880a364c4
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-10 17:16:15 +0000

    criando usuario

--- scenario1/background.sh
+++ scenario1/background.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-sudo apt update
+sudo useradd -m -s /bin/bash aluno
+
+echo "aluno:aluno" | sudo chpasswd
+
+sudo usermod -aG sudo aluno
+
+sudo chmod 0440 /etc/sudoers.d/aluno
 
 exit 0
