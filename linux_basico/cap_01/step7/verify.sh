425a6218d26fdc529c4cf5c38f522fdf45fd32ba
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-21 21:12:41 +0000

    ver18

--- linux_basico/cap_01/step7/verify.sh
+++ linux_basico/cap_01/step7/verify.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-# preservo a resposta do aluno
-cat /tmp/last_output > /tmp/resp
-
 # Captura a saída do comando digitado pelo aluno (única linha, sem as datas e sem espaços excessivos)
-actual_output=$(cat /tmp/resp | tr '\n' ' ' | sed 's/[A-Za-z]\{3\} [0-9]\{1,2\} [0-9]\{1,2\}:[0-9]\{2\}//g' | sed 's/  */ /g; s/^ //; s/ $//')
+actual_output=$(cat /tmp/resposta | tr '\n' ' ' | sed 's/[A-Za-z]\{3\} [0-9]\{1,2\} [0-9]\{1,2\}:[0-9]\{2\}//g' | sed 's/  */ /g; s/^ //; s/ $//')
 
 # Define a saída esperada
 expected_output="total 24K -rw-rw-r-- 1 marcos engenharia 11K xxx xx xx:xx modelo1.docx -rw-rw-r-- 1 ezequiel engenharia 4.4K xxx xx xx:xx modelo2.docx -rw-rw-r-- 1 anderson engenharia 2.9K xxx xx xx:xx modelo3.docx"
