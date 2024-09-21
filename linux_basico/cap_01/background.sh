ce59bfa8f8afd2af0976c76e019ea222a219ae72
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-21 10:17:30 +0000

    files

--- linux_basico/cap_01/background.sh
+++ linux_basico/cap_01/background.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# cria o usuário aluno com a pasta home
+# cria os usuários com a pasta home
 sudo useradd -m -s /bin/bash aluno
 sudo useradd -m -s /bin/bash marcos
 sudo useradd -m -s /bin/bash ezequiel
 sudo useradd -m -s /bin/bash anderson
 
-# cria um grupo 'alunos'
-sudo groupadd alunos
-sudo usermod -aG alunos aluno
-sudo usermod -aG alunos marcos
-sudo usermod -aG alunos ezequiel
-sudo usermod -aG alunos anderson
+# cria um grupo 'engenharia'
+sudo groupadd engenharia
+sudo usermod -aG engenharia aluno
+sudo usermod -aG engenharia marcos
+sudo usermod -aG engenharia ezequiel
+sudo usermod -aG engenharia anderson
 
 # definir a senha para o usuário aluno
 echo "aluno:senha" | sudo chpasswd
@@ -25,6 +25,10 @@ sudo usermod -aG sudo aluno
 # ajustar as permissões
 sudo chmod 0440 /etc/sudoers.d/aluno 
 
+# cria a pasta /files
+sudo mkdir /files
+sudo chown aluno:engenharia /files
+
 # loga como aluno para copiar a pasta com os arquivos
 sudo su - aluno << 'EOF'
 
@@ -36,6 +40,14 @@ unzip repo.zip
 
 # Copia a pasta files para a home do aluno
 cp -r killercoda-main/linux_basico/cap_01/files/* ~/
+
+cp killercoda-main/linux_basico/cap_01/files/arquivo /files/modelo1.docx
+cp killercoda-main/linux_basico/cap_01/files/arquivo.txt /files/modelo2.docx
+cp killercoda-main/linux_basico/cap_01/files/arquivo02 /files/modelo3.docx
+
+chown marcos:engenharia /files/modelo1.docx
+chown ezequiel:engenharia /files/modelo2.docx
+chown anderson:engenharia /files/modelo3.docx
 
 # Remove o arquivo ZIP e a pasta extraída
 rm repo.zip
