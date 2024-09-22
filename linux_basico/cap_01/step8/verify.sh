56d5e6555edcf6b88168cfe4328e40f9fe46b574
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-22 18:10:23 +0000

    rev17

--- linux_basico/cap_01/step8/verify.sh
+++ linux_basico/cap_01/step8/verify.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+# Resposta esperada (exatamente)
+saida_correta=$(cat <<EOF
+-rw-rw-r-- 1 marcos   engenharia  11K Nov 29  2023 /files/modelo1.docx
+-rw-rw-r-- 1 anderson engenharia 4.4K Mar 14  2024 /files/modelo3.docx
+-rw-rw-r-- 1 ezequiel engenharia 2.9K Dec 11  2023 /files/modelo2.docx
+EOF
+)
+
+# linha única sem espaços excessivos
+expected_output=$(echo "$saida_correta" | tr '\n' ' ' | sed 's/  */ /g; s/^ //; s/ $//')
+echo "$expected_output" > /tmp/expected_output
+
 # Preservo a última resposta do aluno
 cat /tmp/last_output > /tmp/resposta
-
-# Captura a saída do comando digitado pelo aluno (única linha, sem as datas e sem espaços excessivos)
-#actual_output=$(cat /tmp/resposta | tr '\n' ' ' | sed 's/[A-Za-z]\{3\} [0-9]\{1,2\} [0-9]\{1,2\}:[0-9]\{2\}//g' | sed 's/  */ /g; s/^ //; s/ $//')
 
 # Captura a saída do comando digitado pelo aluno (única linha e sem espaços excessivos)
 actual_output=$(cat /tmp/resposta | tr '\n' ' ' | sed 's/  */ /g; s/^ //; s/ $//')
 echo "$actual_output" > /tmp/actual_output
-
-# Define a saída esperada
-# Em única linha e sem as datas (porque variam) para facilitarr a comparação.
-expected_output="-rw-rw-r-- 1 marcos engenharia 11K Nov 29 2023 /files/modelo1.docx -rw-rw-r-- 1 anderson engenharia 4.4K Mar 14 2024 /files/modelo3.docx -rw-rw-r-- 1 ezequiel engenharia 2.9K Dec 11 2023 /files/modelo2.docx"
-echo "$expected_output" > /tmp/expected_output
 
 # Verifica se a saída do comando corresponde à saída esperada
 if [[ "$actual_output" == "$expected_output" ]]; then
