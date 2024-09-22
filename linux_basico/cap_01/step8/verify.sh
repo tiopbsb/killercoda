0b45c4de959c3c6cb8720fb1858c98849f4fb074
TI Operacional - CINDACTA I <96258447+tiopbsb@users.noreply.github.com>
2024-09-22 17:14:23 +0000

    rev9

--- linux_basico/cap_01/step8/verify.sh
+++ linux_basico/cap_01/step8/verify.sh
@@ -10,7 +10,7 @@ echo "$actual_output" > /tmp/actual_output
 # Define a saída esperada
 # Em única linha e sem as datas (porque variam) para facilitarr a comparação.
 expected_output="total 24K -rw-rw-r-- 1 marcos engenharia 11K modelo1.docx -rw-rw-r-- 1 anderson engenharia 4.4K modelo3.docx -rw-rw-r-- 1 ezequiel engenharia 2.9K modelo2.docx"
-echo "$expected_output" > /tmp/actual_output
+echo "$expected_output" > /tmp/expected_output
 
 # Verifica se a saída do comando corresponde à saída esperada
 if [[ "$actual_output" == "$expected_output" ]]; then
